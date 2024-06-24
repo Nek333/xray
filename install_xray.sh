967a36357b8185ee421c70a1c4116cda4faa1fd7
Nek333 <125662225+Nek333@users.noreply.github.com>
2024-06-24 14:31:51 +0300

    Update install_xray.sh

--- install_xray.sh
+++ install_xray.sh
@@ -92,5 +92,5 @@ mv /root/xray/temp_config.json /root/xray/config.json
 
 sudo systemctl restart xray
 echo "Скопируйте публичный ключ: $public_key"
-echo "Скопируйте ID: $uuid"
+echo "Скопируйте uuid: $uuid"
 
